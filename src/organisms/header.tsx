d859f49fd92f74e53eb3963faddaf403fc09483f
tatsuyafukui <nakedtatsuya@gmail.com>
2019-06-23 01:36:52 +0900

    initial commit2

---
+++ src/organisms/header.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+
+
+    render() {
+
+        return (
+            <div>
+                <div>
+                    <h1>パズル</h1>
+                </div>
+            </div>
+        )
+
+    }
+
+}
+
+
+
+export default Header;
---
+++ src/pages/top.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Top extends Component {
+
+
+
+    render() {
+
+        return (
+            <div>
+                <h1>header</h1>
+                <h1>image</h1>
+                <h1>upload</h1>
+                <h1>selection</h1>
+            </div>
+        )
+
+    }
+
+}
+
+export default Top;

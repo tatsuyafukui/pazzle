397ef69e7e0d85712e7735f15a1f6a3e937fb599
tatsuyafukui <nakedtatsuya@gmail.com>
2019-07-12 21:02:50 +0900

    deploy test

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com"
+};
+firebase.initializeApp(config);

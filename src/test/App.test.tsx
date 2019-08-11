7b2d33c8d97c01a7d6402ee4e99dd8300291ce9f
tatsuyafukui <nakedtatsuya@gmail.com>
2019-08-11 22:51:03 +0900

    fix ci

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import authReducer from '../modules/auth';
+import collectionReducer from '../modules/collection';
+import pieceReducer from '../modules/pieses';
+
+const reducer = combineReducers({
+  authReducer,
+  collectionReducer,
+  pieceReducer,
+});
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const app = (
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(app, div);
   ReactDOM.unmountComponentAtNode(div);
 });

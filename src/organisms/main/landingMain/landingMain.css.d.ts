b2ef3a190cb54c5bdf3c976db2eecf8580122476
tatsuyafukui <nakedtatsuya@gmail.com>
2019-08-19 10:19:33 +0900

    a

--- src/organisms/main/landingMain/landingMain.css.d.ts
+++ src/organisms/main/landingMain/landingMain.css.d.ts
@@ -19,6 +19,18 @@ declare const styles: {
   readonly "piece3": string;
   readonly "piece4": string;
   readonly "landingLogin": string;
+  readonly "picturesSection": string;
+  readonly "picturesSectionContainer": string;
+  readonly "picturesSectionIcons": string;
+  readonly "picturesSectionIconsItem": string;
+  readonly "square": string;
+  readonly "circleBackground": string;
+  readonly "circleBackgroundBlue": string;
+  readonly "circleBackgroundPeople": string;
+  readonly "circleBackgroundOrange": string;
+  readonly "open": string;
+  readonly "picturesSectionUnderBody": string;
+  readonly "picturesSectionUnderBodyContainer": string;
 };
 export = styles;
 
--- src/organisms/main/landingMain/landingMain.tsx
+++ src/organisms/main/landingMain/landingMain.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import styles from './landingMain.css';
 import speakRectangle from '../../../public/images/speakRectangle.png'
 import piece1 from '../../../public/images/piece1.svg'
@@ -11,8 +11,16 @@ import piece3 from '../../../public/images/piece3.svg'
 import modeImage from '../../../public/images/modeImage.png'
 import piece4 from '../../../public/images/piece4.svg'
 import shareImage from '../../../public/images/shareImage.png'
+import viewing from '../../../public/images/viewing.jpg'
+import character from '../../../public/images/charactor.jpg'
+import design from '../../../public/images/design.jpg'
+import picture from '../../../public/images/picture.jpg'
+import sara from '../../../public/images/sara.jpeg'
+import cat from '../../../public/images/cat.jpg'
+import fuji from '../../../public/images/fuji.jpg'
+
 import { clickLogin } from '../../../modules/auth';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 
 interface IProps {
   title: string;
@@ -20,6 +28,14 @@ interface IProps {
 
 const LandingMain: React.FC = props => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    window.addEventListener('scroll',a)
+    return () => {
+      window.removeEventListener('scroll', a);
+    }
+  },[])
 
   return (
     <main className={styles.main}>
@@ -37,48 +53,92 @@ const LandingMain: React.FC = props => {
                 </div>
               </div>
               <div className={`${styles.time} ${styles.future}`}>
-                <span>Time attack.</span>
-                <img src={speakRectangle} />
+                <span>最速クリアタイムを目指そう！</span>
+                <img src={speakRectangle} alt={'test'} />
               </div>
               <div className={styles.piece1}>
-                <img src={piece1}/>
+                <img src={piece1} alt={'test'}/>
               </div>
               <div className={`${styles.ranking} ${styles.future}`}>
-                <span>Enjoy ranking battles<br/> of cleared times.</span>
-                <img src={speakRectangle} />
+                <span>目指せランキング１位！</span>
+                <img src={speakRectangle} alt={'test'} />
               </div>
               <div className={styles.piece2}>
-                <img src={piece2}/>
+                <img src={piece2} alt={'test'}/>
               </div>
               <div className={`${styles.mode} ${styles.future}`}>
-                <span>Three difficulty <br/>levels are available.</span>
-                <img src={speakRectangle} />
+                <span>難易度は３つから選べます！</span>
+                <img src={speakRectangle} alt={'test'} />
               </div>
               <div className={styles.piece3}>
-                <img src={piece4}/>
+                <img src={piece4} alt={'test'}/>
               </div>
               <div className={`${styles.share} ${styles.future}`}>
-                <span>Share results to social media.</span>
-                <img src={speakRectangle} />
+                <span>完成したパズルとクリアタイムを<br/>SNSでシェアしよう！</span>
+                <img src={speakRectangle}  alt={'test'}/>
               </div>
               <div className={styles.piece4}>
-                <img src={piece3}/>
+                <img src={piece3} alt={'test'}/>
               </div>
             </div>
           </div>
           <div className={styles.floatContents}>
             <div className={styles.timePeace}>
-              <img src={timeImage}/>
+              <img src={timeImage} alt={'test'}/>
             </div>
             <div className={styles.rankingPeace}>
-              <img src={rankingImage}/>
+              <img src={rankingImage} alt={'test'}/>
             </div>
             <div className={styles.modePeace}>
-              <img src={modeImage}/>
+              <img src={modeImage} alt={'test'}/>
             </div>
             <div className={styles.sharePeace}>
-              <img src={shareImage}/>
+              <img src={shareImage} alt={'test'} />
             </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.picturesSection}>
+        <div className={styles.picturesSectionContainer}>
+          <h2>Photo types ...</h2>
+          <div className={styles.picturesSectionIcons}>
+            <div className={styles.picturesSectionIconsItem}>
+              <div className={styles.square}>
+                <img src={viewing} id={'test1'}/>
+                <div className={`${styles.circleBackground} ${styles.circleBackgroundBlue}`}/>
+              </div>
+            </div>
+            <div className={styles.picturesSectionIconsItem}>
+              <div className={styles.square}>
+                <img src={cat}  id={'test2'}/>
+                <div className={`${styles.circleBackground} ${styles.circleBackgroundPeople}`}/>
+              </div>
+            </div>
+            <div className={styles.picturesSectionIconsItem}>
+              <div className={styles.square}>
+                <img src={sara} id={'test3'} />
+                <div className={`${styles.circleBackground} ${styles.circleBackgroundOrange}`}/>
+              </div>
+            </div>
+            <div className={styles.picturesSectionIconsItem}>
+              <div className={styles.square}>
+                <img src={design}  id={'test4'}/>
+                <div className={`${styles.circleBackground} ${styles.circleBackgroundBlue}`}/>
+              </div>
+            </div>
+            <div className={styles.picturesSectionIconsItem}>
+              <div className={styles.square}>
+                <img src={fuji}  id={'test5'}/>
+                <div className={`${styles.circleBackground} ${styles.circleBackgroundPeople}`}/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.picturesSectionUnderBody}>
+          <div className={styles.picturesSectionUnderBodyContainer}>
+            <p id={'test6'}>景色、動物、模様、絵画、パズルに使う題材は自由自在。<br/>
+              お好きなものをアップしてゲームを楽しみましょう。
+            </p>
           </div>
         </div>
       </section>
@@ -87,3 +147,87 @@ const LandingMain: React.FC = props => {
 };
 
 export default LandingMain;
+
+const grayscale = async (image: any) => {
+  var canvas = document.createElement("canvas");
+  var ctx: any = canvas.getContext("2d");
+  var img = new Image();
+
+  const url = await img.addEventListener('load', (e) => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    var pixels: any = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (var y: number = 0; y < pixels.height; y++) {
+      for (var x : number= 0; x < pixels.width; x++) {
+        var i: number = (y * 4) * pixels.width + x * 4;
+        var rgb: number = (pixels.data[i] + pixels.data[i + 1] + pixels.data[i + 2]) / 3;
+        pixels.data[i] = rgb;
+        pixels.data[i + 1] = rgb;
+        pixels.data[i + 2] = rgb;
+      }
+    }
+    ctx.putImageData(pixels, 0, 0, 0, 0, pixels.width, pixels.height);
+
+    // 画像を差し替える
+    console.log(canvas.toDataURL())
+    return canvas.toDataURL();
+  });
+  img.src = image
+};
+
+const a = (event: any) => {
+  const lastScrollY = window.scrollY;//316
+  if(lastScrollY < 300) {
+    return;
+  }
+
+  if(lastScrollY >= 336) {
+    const test: any = document.getElementById('test1');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test1');
+    test.classList.remove(styles.open);
+  }
+
+  if(lastScrollY >= 380) {
+    const test: any = document.getElementById('test2');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test2');
+    test.classList.remove(styles.open);
+  }
+
+  if(lastScrollY >= 430) {
+    const test: any = document.getElementById('test3');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test3');
+    test.classList.remove(styles.open);
+  }
+
+  if(lastScrollY >= 470) {
+    const test: any = document.getElementById('test4');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test4');
+    test.classList.remove(styles.open);
+  }
+
+  if(lastScrollY >= 525) {
+    const test: any = document.getElementById('test5');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test5');
+    test.classList.remove(styles.open);
+  }
+
+  if(lastScrollY >= 600) {
+    const test: any = document.getElementById('test6');
+    test.classList.add(styles.open);
+  }else{
+    const test: any = document.getElementById('test6');
+    test.classList.remove(styles.open);
+  }
+};
